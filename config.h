27fde99826c691a8ea5965faebb612f36d739dd9
Stephen Crane <jscrane@gmail.com>
2024-08-26 08:20:13 +0100

    Diskboot (#14)
    * ...
    
    * booting from disk
    
    * foo
    
    * ...

--- config.h
+++ config.h
@@ -7,6 +7,7 @@
 // ;       ibm-compatible 8" disks
 
 #define DRIVES		4
+#define TRACKS		77
 #define SECLEN		128
 #define SECTRK		26
 
@@ -55,15 +56,23 @@
 #endif
 
 #if FONT == plain_font
+#if !defined(ROWS)
 #define ROWS    	30
+#endif
+#if !defined(COLS)
 #define COLS    	64
+#endif
 #define FONT_W		5
 #define FONT_H		8
 #define FONT_OFF	0x20
 
 #elif FONT == tama_font
+#if !defined(ROWS)
 #define ROWS		30
+#endif
+#if !defined(COLS)
 #define COLS		80
+#endif
 #define FONT_W		4
 #define FONT_H		8
 #define FONT_OFF	0x20
